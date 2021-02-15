89988df1eba5f52aa1600f682858d222a028594f
proraj9088 <rp8735685@gmail.com>
2021-02-16 02:06:59 +0530

    using env port if available
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants')
 const express = require('express') 
 const path = require('path')
 
+const SERVER_PORT = process.env.PORT || 3030
+
 const app = express()
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req,res) => res.send("hello backend"))
 
-app.listen(3030, () => {console.log("Sever Started")})
+app.listen(SERVER_PORT, () => {console.log("Sever Started")})
